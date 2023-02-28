a380df180307d719ccf866ece926ead4c6611fd3
cubdesign <takeo@cubdesign.com>
2023-02-28 15:13:17 +0900

    add graphql

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://beta.pokeapi.co/graphql/v1beta",
+  // documents: "src/**/*.tsx",
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      plugins: ["typescript", "typescript-operations"],
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"],
+    },
+  },
+};
+
+export default config;
